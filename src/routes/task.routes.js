3efa32d078950fe6c6c75a246ff140b235ef3000
angelrosmend <angelrosmend@gmail.com>
2020-06-26 17:20:22 -0300

    Finish part 1 of the tutorial

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,27 +4,42 @@ const router = express.Router();
 const Task = require('../models/task');
 
 
-//Consulting tasks
+//Consulting tasks (GET)
 router.get('/', async (req, res) => {
   const tasks = await Task.find(); //saves fetch in a variable
   console.log(tasks);
   res.json(tasks); //Response to the browser
 });
 
-//Route to add tasks
+//Consulting specific task
+router.get('/:id', async (req, res) => {
+ const task = await Task.findById(req.params.id);
+ res.json(task)
+});
+
+//Route to add new tasks (POST)
 router.post('/', async(req, res) => {
   const {title, description} = req.body;
   const task = new Task({title, description});
-  await task.save()
+  await task.save();
   res.json({status: 'Task Saved'})
 });
 
-//
+//Updating new task (PUT)
 router.put('/:id', async (req, res) => {
  const { title, description } = req.body;
  const newTask = {title, description};
  await Task.findByIdAndUpdate(req.params.id, newTask);
- res.json({status: 'Task uploaded'})
+ res.json({status: 'Task updated'})
 });
+
+//Deleting new task
+router.delete('/:id', async (req, res) => {
+  const { title, description } = req.body;
+  await Task.findByIdAndRemove(req.params.id);
+  res.json({status: 'Task deleted'});
+ });
+
+
 
 module.exports = router; 
